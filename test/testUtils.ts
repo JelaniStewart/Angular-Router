682d9c23d56c37e1f3a72fe4c78251121c646814
Jelani <jelanistewart525@gmail.com>
2024-09-03 08:48:58 +0800

    add test version

---
+++ test/testUtils.ts
@@ -0,0 +1,7 @@
+import { DebugElement } from '@angular/core';
+export function clickOnElement(element: DebugElement, button = 0, metaKey = false, ctrlKey = false) {
+  element.triggerEventHandler('click', { button, metaKey, ctrlKey });
+}
+
+export const tick = () => new Promise((resolve) => setTimeout(resolve));
+
